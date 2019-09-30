9b5c7a66732d8cde5a2a1aea41191418234bba4e
DenMinzalirov <47721911+DenMinzalirov@users.noreply.github.com>
2019-09-30 23:26:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -36,11 +36,26 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
+let reg1 = /\d{1,10}/g;
+let reg2 = /\d{1,2}/g;
 
 function decode(expr) {
     // write your solution here
+    let res = expr.split('**********');
+    return res = res
+        .map((word) => (
+            word.match(reg1)
+                .map((letter) => {
+                    let encoder = letter.match(reg2)
+                        .map((unit) => {
+                            if (unit === '10') return '.';
+                            if (unit === '11') return '-';
+                        }).join('');
+                return MORSE_TABLE[encoder];
+            }).join('')
+        )).join(' ');
 }
-
+console.log(decode('00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010')); 
 module.exports = {
     decode
 }
